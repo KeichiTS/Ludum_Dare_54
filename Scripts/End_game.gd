3c83b108a048f2a5faaba6532286d774670ea1eb
KeichiTS <kkeichi@gmail.com>
2023-10-02 16:20:34 -0300

    html

--- Scripts/End_game.gd
+++ Scripts/End_game.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.is_in_group('player'):
-		get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
+		get_tree().change_scene_to_file("res://Scenes/end_screen.tscn")
